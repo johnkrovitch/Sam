2fe5cd72f9100c60789fea4940dc2e0a2ca2dbc6
Arnaud Frézet <arnaudfrezet@gmail.com>
2017-09-02 00:34:55 +0200

    fix bug when copying directory structure (refs #2)

--- File/Locator.php
+++ File/Locator.php
@@ -9,10 +9,12 @@ use Symfony\Component\Finder\Finder;
 class Locator
 {
     /**
+     * The normalizer associated to the file locator.
+     *
      * @var Normalizer
      */
     protected $normalizer;
-
+    
     /**
      * Locator constructor.
      *
@@ -22,7 +24,7 @@ class Locator
     {
         $this->normalizer = $normalizer;
     }
-
+    
     /**
      * Locate a source either if it a file or a directory and normalize it. Return an array of SplFileInfo.
      *
@@ -34,18 +36,21 @@ class Locator
     {
         $sources = [];
         $normalizedSources = [];
-
+        
         // if the wildcard is at the end of the string, it means that we look for a directory
         $endingWithWilCard = substr($source, strlen($source) - 2) === '/*';
-
+        
         if ($endingWithWilCard) {
             $source = substr($source, 0, strlen($source) - 2);
         }
-
+        
         if (is_dir($source) || $endingWithWilCard) {
             $finder = new Finder();
-            $finder->files()->in($source);
-
+            $finder
+                ->files()
+                ->in($this->removeLastSlash($source))
+            ;
+            
             foreach ($finder as $file) {
                 $sources[] = $file;
             }
@@ -55,27 +60,32 @@ class Locator
         } else {
             $sources[] = $source;
         }
-
+        
         // each found sources should be normalized
         foreach ($sources as $source) {
             $normalizedSources[] = $this
                 ->normalizer
                 ->normalize($source);
         }
-
+        
         return $normalizedSources;
     }
-
+    
     /**
+     * Return the normalizer associated to the file locator.
+     *
      * @return Normalizer
      */
     public function getNormalizer()
     {
         return $this->normalizer;
     }
-
+    
     /**
+     * Return files sources using the finder to allow wild wards.
+     *
      * @param $source
+     *
      * @return SplFileInfo[]
      */
     protected function getSourcesFromFinder($source)
@@ -84,7 +94,7 @@ class Locator
         $filename = array_pop($array);
         $directory = $source;
         $pattern = '*';
-
+        
         // if a dot is present, the last part is the filename pattern
         if (strstr($filename, '.') !== false) {
             $pattern = $filename;
@@ -94,13 +104,29 @@ class Locator
         $finder
             ->name($pattern)
             ->in($directory);
-
+        
         $sources = [];
-
+        
         foreach ($finder as $source) {
             $sources[] = $source;
         }
-
+        
         return $sources;
+    }
+    
+    /**
+     * Remove the last slash of the string.
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    private function removeLastSlash($string)
+    {
+        if ('/' === substr($string, strlen($string) - 1)) {
+            $string = substr($string, 0, strlen($string) - 1);
+        }
+        
+        return $string;
     }
 }
--- Filter/Copy/CopyFilter.php
+++ Filter/Copy/CopyFilter.php
@@ -10,55 +10,66 @@ use Symfony\Component\Filesystem\Filesystem;
 class CopyFilter extends Filter
 {
     /**
+     * Copy the sources files to the destinations.
+     *
      * @param SplFileInfo[] $sources
      * @param string[] $destinations
+     *
      * @return SplFileInfo[]
+     *
      * @throws Exception
      */
     public function run(array $sources, array $destinations)
     {
         $fileSystem = new Filesystem();
-
+        
         // must have the same sources and destinations number
         if (count($sources) !== count($destinations) && count($destinations) !== 1) {
             throw new Exception('Sources and destinations count mismatch');
         }
-
+        
+        // if only one destination is specified for multiple sources, it should be a directory. We should create it if
+        // it is not exists
         if (count($destinations) === 1 && !is_dir($destinations[0]) && count($sources) > 1) {
-            throw new Exception('If only one destination is set for multiple source, it should be a directory');
+            $fileSystem->mkdir($destinations[0]);
         }
         $copiedFiles = [];
-
+        
         // copy generated files to its destination
         foreach ($sources as $index => $source) {
-
+            
             if (array_key_exists($index, $destinations)) {
                 $destination = $destinations[$index];
             } else {
                 $destination = $destinations[0];
             }
-
+            
             if (is_dir($destination)) {
-                $destination = $destination.'/'.$source->getFilename();
+                $destination = $this->removeLastSlash($destination);
+                
+                if ($source instanceof \Symfony\Component\Finder\SplFileInfo && $source->getRelativePath()) {
+                    $destination .= '/'.$source->getRelativePath();
+                }
+                $destination .= '/'.$source->getFilename();
             }
             $this->addNotification('copying "'.$source.'" to "'.$destination.'"');
-
+            
             $fileSystem->copy($source->getRealPath(), $destination);
             $copiedFiles[] = $destination;
         }
-
+        
         return $copiedFiles;
     }
-
+    
     /**
-     * 
+     * No specific requirements for the copy filter.
      */
     public function checkRequirements()
     {
     }
-
+    
     /**
-     * Return the file extensions supported by this filter.
+     * Return the file extensions supported by this filter. All the files can be process by the copy filter.
      *
      * @return string[]
      */
@@ -67,5 +78,21 @@ class CopyFilter extends Filter
         return [
             '*'
         ];
+    }
+    
+    /**
+     * Remove the last slash of the string.
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    private function removeLastSlash($string)
+    {
+        if ('/' === substr($string, strlen($string) - 1)) {
+            $string = substr($string, 0, strlen($string) - 1);
+        }
+        
+        return $string;
     }
 }
